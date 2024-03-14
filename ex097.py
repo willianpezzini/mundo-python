4cdbd60c114adc4ef03171dbeb85933d3a08b18a
Willian Hercilio Pezzini <williantijucas@gmail.com>
2024-03-14 06:25:57 -0300

    exercícios - Mundo 3

---
+++ ex097.py
@@ -0,0 +1,11 @@
+def escreva(msg):
+    tam = len(msg) + 4
+    print('_' * tam)
+    print(f'  {msg}')
+    print('_' * tam)
+
+
+#Programa Principal
+escreva('Willian Hercilio Pezzini')
+escreva('Curso em Vídeo')
+escreva('Oi')
